406d6bc55dbd3b616deb03a27a242965420438a4
Tiare Infante <tiare.infante.snmartin@gmail.com>
2024-11-08 13:16:04 -0300

    Agregando Analytics a page.js

--- app/page.js
+++ app/page.js
@@ -1,24 +1,32 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Button } from "./components/ui/button"
-import { ArrowRight, CheckCircle, Clock, LayoutDashboard, PhoneCall, Menu, X } from "lucide-react"
-import ContactForm from './components/ContactForm'
-import Navbar from './components/navbar'
-import { motion } from 'framer-motion'
-import ElegantHeroSection from './components/home/Home'
-import ServicesSection from './components/home/Servicios'
-import BlogSection from './components/home/BlogSection'
-import Contacto from './components/home/Contact'
-import Footer from './components/home/Footer'
-import EnhancedFeedbackButton from './components/suportchat/Supportchat.jsx'
-
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './components/ui/button';
+import {
+  ArrowRight,
+  CheckCircle,
+  Clock,
+  LayoutDashboard,
+  PhoneCall,
+  Menu,
+  X,
+} from 'lucide-react';
+import ContactForm from './components/ContactForm';
+import Navbar from './components/navbar';
+import { motion } from 'framer-motion';
+import ElegantHeroSection from './components/home/Home';
+import ServicesSection from './components/home/Servicios';
+import BlogSection from './components/home/BlogSection';
+import Contacto from './components/home/Contact';
+import Footer from './components/home/Footer';
+import EnhancedFeedbackButton from './components/suportchat/Supportchat.jsx';
+import Analytics from '../analiticas/analytics';
 
 export default function LandingPage() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [isContactFormOpen, setIsContactFormOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isContactFormOpen, setIsContactFormOpen] = useState(false);
 
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
@@ -27,23 +35,43 @@ export default function LandingPage() {
         <ElegantHeroSection />
         <ServicesSection />
         <BlogSection />
-        <Contacto/>
-        <Footer/>
-        <EnhancedFeedbackButton/>
+        <Contacto />
+        <Footer />
+        <EnhancedFeedbackButton />
         {isMenuOpen && (
           <div className="fixed inset-0 bg-background z-20 md:hidden">
             <div className="flex flex-col items-center justify-center h-full space-y-8">
-              <Link href="#benefits" className="text-2xl" onClick={() => setIsMenuOpen(false)}>Beneficios</Link>
-              <Link href="#advantages" className="text-2xl" onClick={() => setIsMenuOpen(false)}>Ventajas</Link>
-              <Button variant="ghost" onClick={() => {
-                setIsContactFormOpen(true)
-                setIsMenuOpen(false)
-              }}>Contacto</Button>
+              <Link
+                href="#benefits"
+                className="text-2xl"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Beneficios
+              </Link>
+              <Link
+                href="#advantages"
+                className="text-2xl"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Ventajas
+              </Link>
+              <Button
+                variant="ghost"
+                onClick={() => {
+                  setIsContactFormOpen(true);
+                  setIsMenuOpen(false);
+                }}
+              >
+                Contacto
+              </Button>
             </div>
           </div>
         )}
-        {isContactFormOpen && <ContactForm onClose={() => setIsContactFormOpen(false)} />}
+        {isContactFormOpen && (
+          <ContactForm onClose={() => setIsContactFormOpen(false)} />
+        )}
       </main>
+      <Analytics />
     </div>
-  )
+  );
 }
